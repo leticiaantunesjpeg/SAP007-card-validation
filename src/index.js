a3a0185c38e235fb441e158fff303f7e98ed5616
Leticia Antunes Almeida <leticiaantunes95@outlook.com>
2022-01-25 20:28:13 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import validator from './validator.js';
 
+import validator from  './validator.js';
+
 console.log(validator);
+
+//Captura do valor do input pelo botão submit//
+let botão= document.getElementById('submit');
+botão.addEventListener('click', captura, validator);
+
+function captura(){
+    const numeros = document.getElementById('cardNumber').value;
+    let requisitos = numeros.length;
+    if(requisitos == 0){
+        alert('Insira um número');
+        return false;
+    }
+    if(requisitos < 16){
+        alert('Número incompleto');
+        return false;
+    }
+}
