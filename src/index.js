496974010777ea898742b1ca6115b0bbc0838458
Leticia Antunes Almeida <leticiaantunes95@outlook.com>
2022-02-01 13:12:11 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,15 +10,15 @@ document.getElementById("submit").addEventListener("click", function()
     console.log(esconderDígitos)
 
     if (valorNúmerodoCartão == ""){
-        document.getElementById("cardNumber").value=esconderDígitos;
+        document.getElementById("cardNumber").value;
         document.getElementById("resultado").innerHTML="Insira um número válido";
     }  
     else if (result == true){
-        document.getElementById("cardNumber").value=esconderDígitos;
+        document.getElementById("cardNumber").value;
         document.getElementById("resultadoPositivo").innerHTML="O número do cartão é válido";
     }
     else {
-        document.getElementById("cardNumber").value=esconderDígitos;
+        document.getElementById("cardNumber").value;
         document.getElementById("resultadoNegativo").innerHTML="O número do cartão é inválido, revise-o";
     }
     
