d978011289d368e666df0256b7d6b76fa5038438
Leticia Antunes Almeida <leticiaantunes95@outlook.com>
2022-02-04 19:44:33 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,33 +1,30 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
-document.getElementById("submit").addEventListener("click", validar)//ouve-se um evento ao clicar o botão submit e
-                                                                    //redireciono para a função validar
-function validar()
-    {
-    const valorNúmerodoCartão = document.getElementById("cardNumber").value; //variável que armazena o valor do nº do c.
-    const result= validator.isValid(valorNúmerodoCartão)
-    console.log(result)
-   
-    const esconderDígitos = validator.maskify(valorNúmerodoCartão)
-    console.log(esconderDígitos)
-    if (result === true) {                                    
-        document.getElementById("cardNumber").value = esconderDígitos;
-      }                                     //condições criadas para mascarar o valor do input após a validação
-    else (result === false); {
-        document.getElementById("cardNumber").value = esconderDígitos;
-    }
+document.getElementById("submit").addEventListener("click", validar); //ouve-se um evento ao clicar o botão submit e
+//redireciono para a função validar
+function validar() {
+  const valorNúmerodoCartão = document.getElementById("cardNumber").value; //variável que armazena o valor do nº do c.
+  const result = validator.isValid(valorNúmerodoCartão);
+  console.log(result);
 
-    if (valorNúmerodoCartão == ""){ //condição para escrever no html caso o input do cardNumber estiver vazio 
-        document.getElementById("cardNumber").value;
-        document.getElementById("resultado").innerHTML="Insira um número válido";
-    }  
-    else if (result == true){ //condição para escrever no html caso o número do cartão for válido
-        document.getElementById("cardNumber").value;
-        document.getElementById("resultadoPositivo").innerHTML="O número do cartão é válido";
-    }
-    else { //condição para escrever no html caso o número do cartão for inválido
-        document.getElementById("cardNumber").value;
-        document.getElementById("resultadoNegativo").innerHTML="O número do cartão é inválido, revise-o";
-    }
-    
+  const esconderDígitos = validator.maskify(valorNúmerodoCartão);
+  console.log(esconderDígitos);
+  if (result === true) {
+    document.getElementById("cardNumber").value = esconderDígitos;
+  } //condições criadas para mascarar o valor do input após a validação
+
+  if (valorNúmerodoCartão == "") {
+    //condição para escrever no html caso o input do cardNumber estiver vazio
+    document.getElementById("resultado").innerHTML = "Insira um número válido";
+  } else if (result == true) {
+    //condição para escrever no html caso o número do cartão for válido
+    document.getElementById("resultado").innerHTML =
+      "O número do cartão é válido";
+    document.getElementById("resultado").style.color = "green";
+  } else {
+    //condição para escrever no html caso o número do cartão for inválido
+    document.getElementById("resultado").innerHTML =
+      "O número do cartão é inválido, revise-o";
+      document.getElementById("resultado").style.color = "red";
+  }
 }
